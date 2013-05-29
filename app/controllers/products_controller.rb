16f9d06d3d97fd7353489823e10d0436b0dbb92a
Tommy Kail <tommykail@gmail.com>
2013-05-29 14:25:03 -0400

    Generating the products controller and views

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,46 @@
+class ProductsController < ApplicationController
+  
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def create
+    @product = Product.new(params[:product])
+    if @product.save
+      redirect_to products_url
+    else
+      render :new
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update_attributes(params[:product])
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+  end
+end
+
+
+
+
+
