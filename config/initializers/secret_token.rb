48ae7676555dfc5c1dd4042e5e540c9b5e4e2c7f
Tommy Kail <tommykail@gmail.com>
2013-05-29 11:20:51 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RainforestRegrown::Application.config.secret_token = '97c4229b2e049f7a93c4fe309ecc516fe5247664165213254750a60e2d8ee4391fd2c58d2d2a1f0f3f306d2def6da92d3d9b7188c107228049b44afc84120aca'
