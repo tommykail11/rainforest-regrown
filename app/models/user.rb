5182b2d4dc35f53c3f34a193ad30031ad19fdb25
Tommy Kail <tommykail@gmail.com>
2013-05-30 15:12:27 -0400

    Generating review model and using appropriate associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
+
+  has_many :reviews
+  has_many :products :through => :reviews
+
   has_secure_password
   validates_presence_of :password, :on => :create
 end
