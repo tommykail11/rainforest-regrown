2e3817ee1990aaaae84aa2068111f476d20fd71a
Tommy Kail <tommykail@gmail.com>
2013-06-04 00:17:45 -0400

    Writing basic product tests

---
+++ test/unit/product_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class ProductTest < ActiveSupport::TestCase
+  setup do
+    @p = FactoryGirl.create(:product)
+  end
+
+  test "must have a name" do
+    # @p is defined in the setup do block
+    # assigning the name of @p to be blank
+    @p.name = ""
+    # because in model/product.rb, presence of name must be is true
+    # so it is not going to pass validations
+    refute @p.valid? 
+    # refute and assert are the opposite
+    # assert false that would fail the test
+    # refute false means the test passes
+  end
+
+  test "must have a description"
+    @p.description = ""
+    refute @p.valid?
+  end
+
+  test "must have a price"
+    @p.price = ""
+    refute @p.valid?
+  end
+
+  test "price must be an integer"
+    @p.price = 5
+    assert @p.valid?
+  end
+end
